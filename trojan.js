eb79ca699b8c6ae0063ada91d58f99e2df538739
vngbvnhbf <gwertyuio95@gmail.com>
2025-11-29 08:53:51 +0800

    Update trojan.js

--- trojan.js
+++ trojan.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 let subPath = 'link';     // 节点订阅路径,不修改将使用/sub/${UUID}作为订阅路径
 let proxyIP = '13.230.34.30';  // proxyIP
-let yourUUID = '5dc15e15-f285-4a9d-959b-0e4fbdd77b63'; // UUID
+let yourUUID = 'b4577c91-a0e3-4881-937f-1eb93cc8e0bc'; // UUID
 
 let cfip = [ 'mfa.gov.ua#SG','saas.sin.fan#HK','store.ubi.com#JP','cf.130519.xyz#KR','cf.008500.xyz#HK','cf.090227.xyz#SG','cf.877774.xyz#HK','cdns.doon.eu.org#JP','sub.danfeng.eu.org#TW','cf.zhetengsha.eu.org#HK'];
 
